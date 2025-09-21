5432883190c22af0da18831ce59765189012233b
Chris Jenkins <cdpjenkins@gmail.com>
2025-09-21 21:40:30 +0100

    test: Tests for all the other cases covered in the specification
    Haven't done Conjured items yet though.

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -45,6 +45,55 @@ internal class GildedRoseTest {
         app.updateQuality()
         assertEquals(0, app.items[0].quality)
     }
+
+    @Test
+    fun `Aged Bries actually increases in Quality the older it gets`() {
+        val app = GildedRose(listOf(Item("Aged Brie", 2, 0)))
+        app.updateQuality()
+        assertEquals(1, app.items[0].quality)
+    }
+
+    @Test
+    fun `The quality of an item is never more than 50`() {
+        val app = GildedRose(listOf(Item("Aged Brie", 2, 50)))
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun `Sulfuras, being a legendary item, never has to be sold or decreases in quality`() {
+        val app = GildedRose(listOf(Item("Sulfuras, Hand of Ragnaros", 0, 80)))
+        app.updateQuality()
+        assertEquals(80, app.items[0].quality)
+    }
+
+    @Test
+    fun `Backstage passes, like aged brie, increases in quality as its sellIn value approaches`() {
+        val app = GildedRose(listOf(Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)))
+        app.updateQuality()
+        assertEquals(21, app.items[0].quality)
+    }
+
+    @Test
+    fun `Backstage passes quality increases by 2 when there are 10 days or less`() {
+        val app = GildedRose(listOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 25)))
+        app.updateQuality()
+        assertEquals(27, app.items[0].quality)
+    }
+
+    @Test
+    fun `Backstage passes quality increases by 3 when there are 5 days or less`() {
+        val app = GildedRose(listOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 35)))
+        app.updateQuality()
+        assertEquals(38, app.items[0].quality)
+    }
+
+    @Test
+    fun `Backstage passes quality drops to 0 after the concert`() {
+        val app = GildedRose(listOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)))
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+    }
 }
 
 
