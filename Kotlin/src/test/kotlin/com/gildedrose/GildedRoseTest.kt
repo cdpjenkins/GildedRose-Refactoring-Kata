986347a0c5b6b7d3ab70d6341d051e44c12c4793
Chris Jenkins <cdpjenkins@gmail.com>
2025-09-21 21:22:46 +0100

    test: Add first few unit tests

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,14 +6,29 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
+    fun `Item's name remains unchanged`() {
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
 
     }
 
+    @Test
+    fun `sellIn is decremented by 1each day`() {
+        val app = GildedRose(listOf(Item("+5 Dexterity Vest", 10, 20)))
+
+        app.updateQuality()
+        assertEquals(9, app.items[0].sellIn)
+    }
+
+    @Test
+    fun `quality is decrementedby 1 each day when sellIn is greater than 0`() {
+        val app = GildedRose(listOf(Item("+5 Dexterity Vest", 10, 20)))
+
+        app.updateQuality()
+        assertEquals(19, app.items[0].quality)
+    }
 }
 
 
